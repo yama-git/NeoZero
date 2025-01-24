dbd765d40f0e4a1b3ab2767de5a1e36d3b4a1293
shion0401 <260359q@ugs.kochi-tech.ac.jp>
2025-01-24 13:30:33 +0900

    iminashi

--- frontend/src/deletion/deletion.js
+++ frontend/src/deletion/deletion.js
@@ -2,34 +2,50 @@
 import React from 'react'; // React用
 import fontstyles from '../font/font.module.css';
 import { useNavigate } from 'react-router-dom'; // ページ遷移用
+import { useLocation } from 'react-router-dom';
 import styles from './deletion.module.css'; // CSSモジュール(cssファイルかく)
 import Left2Img from '../image/Left2.png'; //259:550
 import Right2Img from '../image/Right2.png'; //259:750
 
 const Deletion = () => {
   //ここから下変える
+  const location = useLocation();
+  const { postId } = location.state || {}; // 状態から投稿IDを取得
+  console.log("デリーとで受け取った",postId);
   const navigate = useNavigate(); // ページ遷移用
 
   const handletop = () => { //「トップページ」ボタン押下 
     navigate('/top'); // トップページに移動
   };
+  const handledeletion = async () => {
+    try {
+      const response = await fetch(`https://neozero.metifie.com/post/delete?post_id=${postId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({post_id: postId }), //postIdを送信
+      });
 
-  const handledeletion = () => { //「消去」ボタン押下 
-    navigate('/mypage'); // マイページに移動
+      if (response.ok) {
+        console.log('投稿が削除されました');
+        navigate('/mypage'); // マイページに移動
+      } else {
+        console.error('削除に失敗しました');
+      }
+    } catch (error) {
+      console.error('エラー:', error);
+    }
   };
+  
+  // const handledeletion = () => { //「消去」ボタン押下 
+  //   navigate('/mypage'); // マイページに移動
+  // };
 
   const inputStyle = {
     fontFamily: 'CraftMincho, serif'
   };
-  const handlead1 = () => {
-    //外部サイトへ飛ぶ(新しいタブで)
-    window.open('https://www.info.kochi-tech.ac.jp/faculty_members/profile_shinomori.shtml', '_blank', 'noopener noreferrer')
-  };
 
-  const handlead2 = () => {
-    //外部サイトへ飛ぶ(新しいタブで)
-    window.open('https://www.info.kochi-tech.ac.jp/faculty_members/profile_shikida.shtml', '_blank', 'noopener noreferrer')
-  };
 
   return (
      <div className={fontstyles.fontFamily}>
@@ -43,15 +59,10 @@ const Deletion = () => {
           トップページへ戻る
         </button>
         <div className={styles.advertisement}>
-        <button
-            className={styles.adbutton}
-              onClick={handlead1}
-            >
           <img
             src={Left2Img} // 広告サンプル
             alt="Left2Img" // 代替テキスト
           />
-          </button>
         </div>
       </div>
 
@@ -70,15 +81,10 @@ const Deletion = () => {
 
       <div className={styles.right}>
         <div className={styles.advertisement2}>
-        <button
-            className={styles.adbutton}
-              onClick={handlead2}
-            >
           <img
             src={Right2Img} // 広告サンプル
             alt="Right2Img" // 代替テキスト
           />
-          </button>
         </div>
       </div>
     </div>
