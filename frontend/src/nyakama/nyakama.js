26c4546cd0643e1457beb9a882e464654cae5b9b
Yuha1022 <147596185+Yuha1022@users.noreply.github.com>
2025-01-27 22:08:03 +0900

    広告遷移先の変更

--- frontend/src/nyakama/nyakama.js
+++ frontend/src/nyakama/nyakama.js
@@ -96,12 +96,14 @@ const Nyakama = () => {
 
   const handlead1 = () => {
     //外部サイトへ飛ぶ(新しいタブで)
-    window.open('https://www.info.kochi-tech.ac.jp/faculty_members/profile_matsuzaki.shtml', '_blank', 'noopener noreferrer')
+    //window.open('https://www.info.kochi-tech.ac.jp/faculty_members/profile_matsuzaki.shtml', '_blank', 'noopener noreferrer')
+    window.open('https://anisupo.jimdofree.com/', '_blank', 'noopener noreferrer')
   };
 
   const handlead2 = () => {
     //外部サイトへ飛ぶ(新しいタブで)
-    window.open('https://www.info.kochi-tech.ac.jp/faculty_members/profile_takata.shtml', '_blank', 'noopener noreferrer')
+    //window.open('https://www.info.kochi-tech.ac.jp/faculty_members/profile_takata.shtml', '_blank', 'noopener noreferrer')
+    window.open('https://anisupo.jimdofree.com/', '_blank', 'noopener noreferrer')
   };
 
   return (
